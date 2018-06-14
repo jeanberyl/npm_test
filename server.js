f45668b80dbbac20ae2179e353a2113581a551d5
jeanberyl <jbe14z@gmail.com>
2018-06-14 17:15:42 +0200

    repsosnose

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const Hapi = require('hapi');
+const request = require('request');
 
 
 // Create a server with a host and port
@@ -41,7 +42,6 @@ server.route({
     path: '/',
     handler: (request, h) => {
 
-        var request = require('request');
         //https://societeinfo.com/app/rest/api/v1/querysearch/companies/json?query=IDENTIQ&where=93100&limit=10&key=g4onk62np2m1a7q5co2engenbf3u3itbg3ggnfkbcfk6367sddp
         //https://societeinfo.com/app/rest/api/v1/company/json?registration_number=493361372&key=g4onk62np2m1a7q5co2engenbf3u3itbg3ggnfkbcfk6367sddp
         request('https://societeinfo.com/app/rest/api/v1/querysearch/companies/json?query=IDENTIQ&where=93100&limit=10&key=g4onk62np2m1a7q5co2engenbf3u3itbg3ggnfkbcfk6367sddp', function (error, response, body) {
@@ -57,16 +57,16 @@ server.route({
 
             request('https://societeinfo.com/app/rest/api/v1/company/json?registration_number='+registration_number+'&key=g4onk62np2m1a7q5co2engenbf3u3itbg3ggnfkbcfk6367sddp', function (error, response, body) {
             
-            console.log('error:', error); // Print the error if one occurred
-            console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-            console.log('body:', body); // Print the HTML for the Google homepage.
-            var obj = JSON.parse(body);
+                console.log('error:', error); // Print the error if one occurred
+                console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+                console.log('body:', body); // Print the HTML for the Google homepage.
+                var obj = JSON.parse(body);
 
-            var ape_code_level2 = obj.result.ape_code_level2.slice(0, 1);
+                var ape_code_level2 = obj.result.ape_code_level2.slice(0, 1).toString();
 
-            return Code_Naf[ape_code_level2];
-            
-            console.log(ape_code_level2);
+                return h.response(Code_Naf[ape_code_level2]);
+                
+                console.log(ape_code_level2);
             });
 
          });
